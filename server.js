43c4f0bff3f2bf8d42f67e934137b9284c8eacc3
rebvar-ebra <rebwar.ebrahimi@gmail.com>
2023-05-27 00:02:18 +0200

    client

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
-const express = require("express");
-const expressLayout = require("express-ejs-layouts");
-
-require("dotenv").config();
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const fileUpload = require('express-fileupload');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use(expressLayout);
-app.set("layout", "./layouts/main");
-app.set("view engine", "ejs");
+const port = process.env.PORT || 3000;
 
-const routes = require("./server/routes/recipteRoutes");
-app.use("/", routes);
-app.listen(PORT, () => {
-  console.log(`Listening to port ${PORT}`);
-});
+require('dotenv').config();
+
+app.use(express.urlencoded( { extended: true } ));
+app.use(express.static('public'));
+app.use(expressLayouts);
+
+app.use(cookieParser('CookingBlogSecure'));
+app.use(session({
+  secret: 'CookingBlogSecretSession',
+  saveUninitialized: true,
+  resave: true
+}));
+app.use(flash());
+app.use(fileUpload());
+
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+const routes = require('./server/routes/recipteRoutes.js')
+app.use('/', routes);
+
+app.listen(port, ()=> console.log(`Listening to port ${port}`));
--- server/routes/recipteRoutes.js
+++ server/routes/recipteRoutes.js
@@ -1,8 +1,17 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const recipeControler = require("../controlers/recipeController");
+const recipeController = require('../controllers/recipeController');
 
-router.get("/", recipeControler.homepage);
-router.get("/explore",recipeControler.exploreCategories);
-
+/**
+ * App Routes
+*/
+router.get('/', recipeController.homepage);
+router.get('/recipe/:id', recipeController.exploreRecipe );
+router.get('/categories', recipeController.exploreCategories);
+router.get('/categories/:id', recipeController.exploreCategoriesById);
+router.post('/search', recipeController.searchRecipe);
+router.get('/explore-latest', recipeController.exploreLatest);
+router.get('/explore-random', recipeController.exploreRandom);
+router.get('/submit-recipe', recipeController.submitRecipe);
+router.post('/submit-recipe', recipeController.submitRecipeOnPost);
 module.exports = router;
